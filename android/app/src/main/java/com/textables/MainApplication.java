e75e616b678c80221aabf1d093e19f06984feb52
triyanarief <triyanarief@icloud.com>
2017-04-05 22:39:07 +0700

    my xcode not longer support :(

--- android/app/src/main/java/com/textables/MainApplication.java
+++ android/app/src/main/java/com/textables/MainApplication.java
@@ -3,10 +3,6 @@ package com.textables;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.i18n.reactnativei18n.ReactNativeI18n;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.learnium.RNDeviceInfo.RNDeviceInfo;
-import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -26,11 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new ReactNativeI18n(),
-            new VectorIconsPackage(),
-            new RNDeviceInfo(),
-            new ReactNativeConfigPackage()
+          new MainReactPackage()
       );
     }
   };
